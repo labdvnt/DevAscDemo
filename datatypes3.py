58e69f31c3367f9470e03bbb7925ff0ec5bc0f44
omer.dartar <omerdartaar@gmail.com>
2025-07-05 01:33:41 +0300

    Update Person instance age and enhance output with greeting and birthday celebration methods

--- datatypes3.py
+++ datatypes3.py
@@ -2,10 +2,14 @@
 from PythonClass.challangeClassFunction import Person
 
 # Create an instance of the Person class
-person = Person("Omer", "Dartar", 25, 180)
+person = Person("Omer", "Dartar", 32, 180)
 
 print(person.__dict__)
 
 print(f"Person's name: {person.first_name} {person.last_name}")
 print(f"Person's age: {person.age}")
 print(f"Person's height: {person.height_cm} cm")
+
+person.greet()
+person.celebrate_birthday()
+person.jump()
