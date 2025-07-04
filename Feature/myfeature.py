91b6b6262683102160be75eff58b6dd9b688e976
omer.dartar <omerdartaar@gmail.com>
2025-07-04 22:12:49 +0300

    Add feature messages to myfeature.py

--- Feature/myfeature.py
+++ Feature/myfeature.py
@@ -13,3 +13,5 @@ if __name__ == "__main__":
     # Example usage
     user_name = input("Enter your name: ")
     print(greet_user(user_name))
+    print("This is my feature script.")
+    print("Welcome to the DevAsc feature demo!")
