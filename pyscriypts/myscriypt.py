bc3b1db464cfe2118105bb99db3c26d21914bbb5
omer.dartar <omerdartaar@gmail.com>
2025-07-04 21:38:23 +0300

    This is my third commit

--- pyscriypts/myscriypt.py
+++ pyscriypts/myscriypt.py
@@ -3,6 +3,8 @@ def main():
     Entry point for the script.
     """
     print("Hello, World!")
+    print("Welcome to my script! DevAsc")
+    print("This is my third commit")
 
 if __name__ == "__main__":
     main()
