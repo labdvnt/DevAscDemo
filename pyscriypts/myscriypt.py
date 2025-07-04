5477318421fe4cf7475175582b93b33ad6eef8a7
omer.dartar <omerdartaar@gmail.com>
2025-07-04 21:39:17 +0300

    Add fourth greeting message to the main function

--- pyscriypts/myscriypt.py
+++ pyscriypts/myscriypt.py
@@ -5,6 +5,8 @@ def main():
     print("Hello, World!")
     print("Welcome to my script! DevAsc")
     print("This is my third commit")
+    print("This is my 4. commit")
+
 
 if __name__ == "__main__":
     main()
